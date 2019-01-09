e5e9d4d47abaecada987ec6d2b399b2275424f9b
Aaron Dorrance <adorranc@nd.edu>
2019-01-09 14:51:01 -0500

    Added Message queue functionality to Input

--- src/InputClasses/Input.java
+++ src/InputClasses/Input.java
@@ -14,13 +14,14 @@ public class Input
 {
     private static long m_lWindow;
 
-    private static ArrayList<ArrayList<Message>> m_messageQueues;
+    private static ArrayList<ArrayList<MessagePackage>> m_messageQueues;
 
     private static boolean [] m_bKeyPressed;
     private static boolean [] m_bMouseButtonPressed;
 
     private static String m_strTextBuffer;
     private static boolean m_bRecordingText;
+
     private static int m_iPixelWidth, m_iPixelHeight;
     private static int m_iWidth, m_iHeight;
     private static int m_iXMousePos, m_iYMousePos;
@@ -32,12 +33,38 @@ public class Input
         m_bKeyPressed = new boolean[GLFW_KEY_LAST + 1];
         m_bMouseButtonPressed = new boolean[GLFW_MOUSE_BUTTON_LAST +1];
 
-        m_messageQueues = new ArrayList<>(0);
+        m_strTextBuffer = "";
+        m_bRecordingText = false;
+
+        int [] width = new int[1];
+        int [] height = new int[1];
+        glfwGetFramebufferSize(lWindow, width, height);
+        m_iPixelWidth = width[0];
+        m_iPixelHeight = height[0];
+        glfwGetWindowSize(lWindow, width, height);
+        m_iWidth = width[0];
+        m_iHeight = height[0];
+
+        m_iXMousePos = 0;
+        m_iYMousePos = 0;
+
+
+
+        m_messageQueues = new ArrayList<>(1);
     }
 
     public static void update()
     {
         glfwPollEvents();
+        MessagePackage message = getMessage(0);
+        while (message != null)
+        {
+            if (message.getMesssage() == Message.KEYDOWN && message.getID() == GLFW_KEY_BACKSPACE)
+            {
+                m_strTextBuffer = m_strTextBuffer.substring(0, m_strTextBuffer.length() - 2);
+            }
+            message = getMessage(0);
+        }
     }
 
     public static int createMessageQueue()
@@ -46,9 +73,13 @@ public class Input
         return m_messageQueues.size() -1;
     }
 
-    public static boolean getMessage(int iQueue, IDPackage message, boolean bRemove)
+    public static MessagePackage getMessage(int iQueue)
     {
-        return false;
+        ArrayList<MessagePackage> queue = m_messageQueues.get(iQueue);
+        if (queue.isEmpty())
+            return null;
+
+        return queue.remove(0);
     }
 
 
