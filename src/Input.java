428106ec6f0522f12918bfc9084a1234f9f5496c
Aaron Dorrance <adorranc@nd.edu>
2019-01-09 12:43:04 -0500

    Broke up Message into MessageData and Message enum

--- src/Input.java
+++ src/Input.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import static org.lwjgl.glfw.GLFW.*;
+
 /**
  * This is a class that will handle input from glfw.
  *
@@ -20,4 +22,25 @@ public class Input
     private static int m_iPixelWidth, m_iPixelHeight;
     private static int m_iWidth, m_iHeight;
     private static int m_iXMousePos, m_iYMousePos;
+
+
+    public static void init(long lWindow)
+    {
+        m_lWindow = lWindow;
+        m_bKeyPressed = new boolean[GLFW_KEY_LAST + 1];
+        m_bMouseButtonPressed = new boolean[GLFW_MOUSE_BUTTON_LAST +1];
+
+        m_messageQueues = new ArrayList<>(0);
+    }
+
+    private static void keyCallback(long window, int key, int scancode, int action, int mods)
+    {
+        int iKeyFlag;
+
+        switch (mods)
+        {
+            case GLFW_MOD_SHIFT:
+        }
+    }
+
 }
---
+++ src/MessageData.java
@@ -0,0 +1,15 @@
+public class MessageData
+{
+    public int message;
+    public int iXData;
+    public int iYData;
+
+
+
+    MessageData(int m, int x, int y)
+    {
+        message = m;
+        iXData = x;
+        iYData = y;
+    }
+}
